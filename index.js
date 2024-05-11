abcdcc50c356162841ffd77687c7b2a9843b038d
joweldas1 <work.jowel@gmail.com>
2024-05-11 10:10:02 +0600

    order data store done

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4mwwnz0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4mwwnz0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = "mongodb://localhost:27017"
+       const uri=  "mongodb://localhost:27017/?directConnection=true"
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -55,6 +57,12 @@ async function run() {
       const result = await customerOrder.insertOne(order)
       res.send(result)
       console.log(order);
+    })
+
+
+    app.get('/ordered',async(req,res)=>{
+     const result = await customerOrder.find().toArray()
+     res.send(result)
     })
 
 
